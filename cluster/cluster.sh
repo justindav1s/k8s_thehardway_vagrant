6339bd06783dc16c9e3c9c85afb3170e1bc38984
Justin Davis <justinndavis@gmail.com>
2020-02-08 17:15:17 +0000

    vm management

--- cluster/cluster.sh
+++ cluster/cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -x
 SERVERS="lb controller0 controller1 controller2 worker0 worker1 worker2"
 ACTION=$@
 
@@ -8,6 +8,6 @@ for SERVER in $SERVERS; do
     echo $ACTION : $SERVER
     rm -rf logs/$SERVER.out
     nohup vagrant $ACTION $SERVER > logs/$SERVER.out & 
-    sleep 2
+    sleep 10
 
 done
